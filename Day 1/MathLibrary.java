d46becf9014b34f1cc10030e432c4118da15b4cf
Anirudh1103 <cmanirudh03@gmail.com>
2025-02-21 13:41:06 +0530

    Day 1 practice programs added

---
+++ Day 1/MathLibrary.java
@@ -0,0 +1,7 @@
+public class MathLibrary {
+    public static void main(String[] args) {
+        System.out.println(Math.min(100,10));
+        System.out.println(Math.max(100,10));
+        System.out.println(Math.random());
+    }
+}
